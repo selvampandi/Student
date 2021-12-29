2e180c946da4f3ecd6a2933022506ff89971a493
selvampandi <pandikumar7498@gmail.com>
2021-12-29 09:04:51 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student
+{
+int mark1, mark2;
+public static void main(String[] args)
+{
+Student s1 = new Student();
+	s1.calculateTotal();
+}
+public void calculateTotal()
+{
+	int m1 =90, m2=100;
+	int result = m1+m2;
+	System.out.println(result);
+	
+}
+	
+}
